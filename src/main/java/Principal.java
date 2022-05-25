5c7b92558566d1ec3dc210b411f406b5a744c9ce
CristianJucoski <cristian.jucoski@gmail.com>
2022-05-24 23:23:58 -0300

    Commit Alteração compiler.

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -10,14 +10,18 @@ public class Principal {
         while (!opcao.equals("9")) {
             opcao = JOptionPane.showInputDialog("1 - Leitura \n 2 - Soma\n 3 - Subtração\n 4 - Produto\n 5 - Divisão \n 9 - Sair");
             switch (Integer.parseInt(opcao)) {
-                case 1 -> {
+                case 1:
                     calculadora.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digite o Valor A")));
                     calculadora.setValorB(Double.parseDouble(JOptionPane.showInputDialog("Digite o Valor B")));
-                }
-                case 2 -> JOptionPane.showMessageDialog(null, "Soma: " + calculadora.getAdicao());
-                case 3 -> JOptionPane.showMessageDialog(null, "Subtração: " + calculadora.getSubtracao());
-                case 4 -> JOptionPane.showMessageDialog(null, "Produto: " + calculadora.getProduto());
-                case 5 -> JOptionPane.showMessageDialog(null, "Divisão: " + calculadora.getDivisao());
+                    break;
+                case 2: JOptionPane.showMessageDialog(null, "Soma: " + calculadora.getAdicao());
+                break;
+                case 3: JOptionPane.showMessageDialog(null, "Subtração: " + calculadora.getSubtracao());
+                break;
+                case 4: JOptionPane.showMessageDialog(null, "Produto: " + calculadora.getProduto());
+                break;
+                case 5: JOptionPane.showMessageDialog(null, "Divisão: " + calculadora.getDivisao());
+                break;
             }
         }
     }
